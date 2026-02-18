d2277d1984f46685c91c4fb0076be556173adb50
Egoriik <egor.bazylev07@gmail.com>
2026-02-18 23:25:12 +0300

    commit2

--- 4.py
+++ 4.py
@@ -1,14 +1,20 @@
-#В целом все утраивает, но хочется добавить повторный ввод пароля(passA)
-#Но не совсем понимаю куда его тыкать и через что, была мысль прописать через
-#wlile not az and not AZ and num < 2
-#после первого else, но код вовсе полетел...
+# В целом все утраивает, но хочется добавить повторный ввод пароля(passA)
+# Но не совсем понимаю куда его тыкать и через что, была мысль прописать через
+# wlile not az and not AZ and num < 2
+# после первого else, но код вовсе полетел...
+
+"commit2"
+#добавил строку с while как и хотел, проверяю первое условие,
+#но как только условие 6 и более выполняется он идет дальше
+#не до конца понимаю почему так
 
 passA = input("Введите пароль: ")
-cnt = 0
 az = False
 AZ = False
 num = 0
-if len(passA)>=6:
+cntSim = False
+if len(passA) >= 6:
+    cntSim = True
     for i in passA:
         if "a" <= i <= "z":
             az = True
@@ -16,15 +22,22 @@ if len(passA)>=6:
             AZ = True
         elif "0" <= i <= "9":
             num += 2
-    if az and AZ and num >= 2:
+    if az and AZ and num >= 2 and cntSim:
         print("Пароль принят")
-    else:
+else:
+    while not az and not AZ and num < 2 and not cntSim:
+        if not cntSim:
+            print("В пароле должно быть шесть и более символов")
         if not az:
             print("В пароле должны быть строчные буквы")
         if not AZ:
             print("В пароле должны быть заглавыне буквы")
         if num < 2:
             print("В пароле должно быть больше двух цифр")
+            break
+    passA = input("Введите измененный пароль: ")
+
+
 passB = input("Введите пароль еще раз: ")
 if passA == passB:
     print("Пароль верный!")
@@ -32,8 +45,3 @@ else:
     while passA != passB:
         passB = input("Повторите попытку: ")
     print("Пароль верный!")
-
-
-
-
-
