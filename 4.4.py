afd52b5c815b362f969b154b0d3fa88a58a3a472
Egoriik <egor.bazylev07@gmail.com>
2026-02-25 14:02:17 +0300

    Ввод через числа

--- 4.4.py
+++ 4.4.py
@@ -1,19 +1,19 @@
 print("Введите один из цветов:")
 
 while True:
-    print("-- Красный")
-    print("-- Синий")
-    print("-- Желтый")
-    color = input("Впишите название выбранного цвета: ")
-    SecondColor = input("Впинишите название вторичного цвета: ")
-    if color == "Красный" and SecondColor == "Синий":
+    print("1 - Красный")
+    print("2 - Синий")
+    print("3 - Желтый")
+    color = input("Впишите название или цифру выбранного цвета: ")
+    SecondColor = input("Впишите название или цифру вторичного цвета: ")
+    if color == "Красный" or "1" and SecondColor == "Синий" or "2":
         print("В результате смешивания получится фиолетовый")
         break
-    elif color == "Красный" and SecondColor == "Желтый":
+    elif color == "Красный" or "1" and SecondColor == "Желтый" or "3":
         print("В результате смешивания получится оранжевый")
         break
-    elif color == "Синий" and SecondColor == "Желтый":
+    elif color == "Синий" or "2" and SecondColor == "Желтый" or "3":
         print("В результате смешивания получится зеленый")
         break
     else:
-        print("Вы ввели неккоректное название цвета!")
+        print("Вы ввели некорректное название цвета!")
